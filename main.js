e7235ee897f8f4045035e6cf00f27b74ea981575
JHCWColin-Workstation <jhcwcolin.workstation@gmail.com>
2026-06-22 14:47:12 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      // 如果有 preload 脚本可以在这里指定
+    }
+  });
+
+  // 加载你的前端页面（仓库里已有的 index.html）
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
